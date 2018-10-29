5385101baafc5f56225ad8fa1d7c45ac4e5c7cb4
ollerus <24956184+ollerus@users.noreply.github.com>
2018-10-30 00:28:47 +0300

    Update xfce-configuration.nix

--- xfce-configuration.nix
+++ xfce-configuration.nix
@@ -6,18 +6,24 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-  #hardware.opengl.enable = true;
-  #hardware.opengl.driSupport = true;
+  #boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "/dev/sda"; # or "nodev" for efi only
+    #efiSupport = true;
+    #efiInstallAsRemovable = true;
+  };  
   
-
+  hardware ={
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl vaapiVdpau ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [ vaapiIntel libvdpau-va-gl vaapiVdpau ];
+      s3tcSupport = true;
+  };
 
   networking.hostName = "ollerus"; # Define your hostname.
   networking.networkmanager.enable = true;
@@ -30,12 +36,14 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
- 
+  
   programs = {
     #gnupg.agent = {
     #  enable = true;
     #  enableSshSupport = true;
     #};
+    #mtr.enable = true;
+    java.enable = true;
     zsh = {
       enable = true;
       enableCompletion = true;
@@ -44,7 +52,7 @@
       interactiveShellInit = "ZSH_TMUX_AUTOSTART=true";
       ohMyZsh = {
         enable = true;
-        theme = "gentoo";
+        theme = "agnoster";
         plugins = [ "git" "tmux" ];
       };
     };
@@ -55,12 +63,10 @@
     unzip ntfs3g xarchiver
     gnupg gnupg1compat
     git cmake gnumake gcc
-    firefox vlc screenfetch
-    linuxPackages.acpi_call wget
+    firefox vlc screenfetch wget
     djview xpdf rsync ffmpeg-full
     python3 python36Packages.virtualenv python36Packages.pip
     chromium pavucontrol
-    xfce4-13.xfce4-xkb-plugin
   ];
 
   # Select internationalisation properties.
@@ -90,21 +96,7 @@
       serif = [ "Noto Serif" ];
     };
   };
-
-  # Set your time zone.
-  time.timeZone = "Europe/Moscow";
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  #services.openssh.enable = true;
   
-  #Users
   users.extraUsers.obliq = {
     isNormalUser = true;
     name = "obliq";
@@ -127,12 +119,7 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound.
   sound.enable = true;
-  #hardware.pulseaudio.enable = true;
   hardware.pulseaudio = {
     enable = true;
     package = pkgs.pulseaudioFull;
@@ -143,27 +130,40 @@
 
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.xkbModel = "microsoft";
-  services.xserver.xkbVariant = "winkeys";
-  services.xserver.layout = "us,ru(winkeys)";
-  services.xserver.xkbOptions = "grp:caps_toggle";
-  services.xserver.videoDrivers = [ "intel" ];
+  services.xserver = {
+    enable = true;
+    xkbModel = "microsoft";
+    xkbVariant = "winkeys";
+    layout = "us,ru(winkeys)";
+    xkbOptions = "grp:caps_toggle";
+    videoDrivers = [ "intel" ];
+    libinput.enable = true;
+    desktopManager.enlightenment.enable = true;
+    desktopManager.default = "Enlightenment";
+    displayManager.auto = { enable = true; user = "obliq"; };
+  };
   
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-
-  # Enable Desktop Environment.
-  services.xserver.desktopManager.xfce.enable = true;
-  services.xserver.desktopManager.default = "xfce";
-
-  services.xserver.displayManager.auto.enable = true;
-  services.xserver.displayManager.auto.user = "obliq";
-
-  services.acpid.enable = true;
-  services.tlp.enable = true;
-
-  system.stateVersion = "18.09"; # Did you read the comment?
-  system.autoUpgrade.enable = true;
-
+  services.compton = {
+    enable = true;
+    vSync = "opengl";
+    shadow = true;
+    menuOpacity = "0.9";
+    shadowOpacity = "0.3";
+  };
+  
+  services = {
+    acpid.enable = true;
+    tlp.enable = true;
+    illum.enable = true;
+    printing.enable = true; # Enable CUPS
+    openssh.enable = true;
+  };
+  
+  time.timeZone = "Europe/Moscow";
+  
+  system = {
+    stateVersion = "18.09";
+    autoUpgrade = {
+      enable = true;
+  };
 }
