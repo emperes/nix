6eaf37f68b19a90409b62fb3a0f5208ad1c1c0a1
ollerus <24956184+ollerus@users.noreply.github.com>
2018-10-30 19:14:24 +0000

    Update i18n.nix

--- i18n.nix
+++ i18n.nix
@@ -3,10 +3,8 @@
     consoleFont = "UniCyr_8x16";
     consoleKeyMap = "ruwin_cplk-UTF-8";
     defaultLocale = "ru_RU.UTF-8";
-    consoleColors = [ "000000" "dc322f" "859900" "b58900" 
-                      "268bd2" "d33682" "2aa198" "eee8d5" 
-                      "002b36" "cb4b16" "586e75" "657b83"
-                      "839496" "6c71c4" "93a1a1" "fdf6e3" ];
+    consoleColors = [ "161616" "a65353" "909653" "bd9c5a" "5f788c" "816b87" "688c88" "c5c5c5"
+                      "4a4a4a" "cc6666" "b5bd68" "f0c674" "81a2be" "b294bb" "8abeb7" "f7f7f7" ];
   };
   fonts = {
     fonts = with pkgs; [ noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk ];
