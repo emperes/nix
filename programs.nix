a7c21d0a1d91dadac1e0d5aa995b242385b48d4f
ollerus <24956184+ollerus@users.noreply.github.com>
2018-10-30 18:25:03 +0000

    Update programs.nix

--- programs.nix
+++ programs.nix
@@ -1,14 +1,12 @@
 }
   nixpkgs.config = { allowBroken = true; allowUnfree = true; }
   programs = {
-    rootston.enable = true;
-    sway.enable = true;
-    java.enable = true;
     #gnupg.agent = {
     #  enable = true;
     #  enableSshSupport = true;
     #};
     #mtr.enable = true;
+    java.enable = true;
     zsh = {
       enable = true;
       enableCompletion = true;
@@ -17,9 +15,8 @@
       interactiveShellInit = "ZSH_TMUX_AUTOSTART=true";
       ohMyZsh = {
         enable = true;
-        theme = "gentoo";
+        theme = "agnoster";
         plugins = [ "git" "tmux" ];
       };
     };
   };
-}
