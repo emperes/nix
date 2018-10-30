302a1b858ab5558747de950996f67d687a66f5a2
ollerus <24956184+ollerus@users.noreply.github.com>
2018-10-30 18:08:27 +0000

    Update networking.nix

--- networking.nix
+++ networking.nix
@@ -1,12 +1,12 @@
 {
   networking = {
     hostName = "ollerus";
-     networkmanager.enable = true;
-     wireless.enable = true;
-     #firewall.allowedTCPPorts = [ ... ];
-     #firewall.allowedUDPPorts = [ ... ];
-     #firewall.enable = false;
-     #proxy.default = "http://user:password@proxy:port/";
-     #proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    networkmanager.enable = true;
+    wireless.enable = true;
+    #firewall.allowedTCPPorts = [ ... ];
+    #firewall.allowedUDPPorts = [ ... ];
+    #firewall.enable = false;
+    #proxy.default = "http://user:password@proxy:port/";
+    #proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   };
 }
