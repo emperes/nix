c2944168dd03f5b76b6f625c4a04a4c5615f4258
ollerus <24956184+ollerus@users.noreply.github.com>
2018-10-30 19:41:43 +0000

    Create services.nix

---
+++ services.nix
@@ -0,0 +1,55 @@
+{
+  services.xserver.enable = true;
+  ##################################################
+  #KEYBOARD                                        #
+  services.xserver.xkbModel = "microsoft";         #
+  services.xserver.xkbVariant = "winkeys";         #
+  services.xserver.layout = "us,ru(winkeys)";      #
+  services.xserver.xkbOptions = "grp:caps_toggle"; #
+  ##################################################
+  
+  ##########################################
+  #SYNAPTIC/TOUCHPAD                       #
+  services.xserver.libinput.enable = true; #
+  ##########################################
+  
+  ##############################################################
+  #DE                                                          #
+  services.xserver.desktopManager.enlightenment.enable = true; #
+  services.xserver.desktopManager.default = "Enlightenment";   #
+  ##############################################################
+  
+  ############################################################################
+  #DM                                                                        #
+  #services.xserver.displayManager.lightdm.enable = true;                    #     
+  #services.xserver.displayManager.lightdm.autoLogin.enable = true;          #
+  #services.xserver.displayManager.lightdm.autoLogin.user = "obliq";         #
+  services.xserver.displayManager.auto = { enable = true; user = "obliq"; }; #
+  ############################################################################
+  
+  ##############################################
+  #GRAPHIC                                     #
+  services.xserver.videoDrivers = [ "intel" ]; #
+  services.compton.enable = true;              #
+  services.compton.vSync = "opengl";           #
+  services.compton.shadow = true;              #
+  services.compton.menuOpacity = "0.9";        #
+  services.compton.shadowOpacity = "0.3";      #
+  ##############################################
+  
+  ###############################
+  #LAPTOP                       #
+  services.acpid.enable = true; #
+  services.tlp.enable = true;   #
+  services.illum.enable = true; #
+  ###############################
+  
+  services.printing.enable = true; # CUPS
+  services.openssh.enable = true;
+  services.dbus.enable = true;
+  services.cron.enable = true;
+  services.locate.enable = true;
+  services.udisks2.enable = true;
+  services.ntp.enable = true;
+  #services.nixosManual.showManual = true;
+}
