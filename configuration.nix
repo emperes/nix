99bedee27d90d781822f20da7a008cdb4ca87c7c
ollerus <24956184+ollerus@users.noreply.github.com>
2018-10-31 09:40:38 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -79,7 +79,7 @@
     sway.enable = true;
     java.enable = true;
     adb.enable = true;
-    info.enable = true;
+    documentation.info.enable = true;
     zsh = {
       enable = true;
       enableCompletion = true;
