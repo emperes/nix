f5b36770e58720362461f4ae2e14577a0c8e2602
ollerus <24956184+ollerus@users.noreply.github.com>
2018-10-31 09:24:39 +0000

    Update configuration-lxqt.nix

--- configuration-lxqt.nix
+++ configuration-lxqt.nix
@@ -41,8 +41,10 @@
     consoleFont = "UniCyr_8x16";
     consoleKeyMap = "ruwin_cplk-UTF-8";
     defaultLocale = "ru_RU.UTF-8";
-    consoleColors = [ "1C1B19" "EF2F27" "519F50" "FBB829" "2C78BF" "E02C6D" "0AAEB3" "918175"
-                      "2D2C29" "F75341" "98BC37" "FED06E" "68A8E4" "FF5C8F" "53FDE9" "FCE8C3" ];
+    consoleColors = [ "1C1B19" "EF2F27" "519F50" "FBB829"
+                      "2C78BF" "E02C6D" "0AAEB3" "918175"
+                      "2D2C29" "F75341" "98BC37" "FED06E"
+                      "68A8E4" "FF5C8F" "53FDE9" "FCE8C3" ];
   };
   fonts = {
     fonts = with pkgs; [ noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk ];
