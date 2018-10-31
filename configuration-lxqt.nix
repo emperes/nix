69d8e8a58a9b17974b8977c5cdd3a5366d911c62
ollerus <24956184+ollerus@users.noreply.github.com>
2018-10-31 09:16:41 +0000

    Update configuration-lxqt.nix

--- configuration-lxqt.nix
+++ configuration-lxqt.nix
@@ -1,10 +1,6 @@
 { config, pkgs, ... }:
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
+  imports = [ ./hardware-configuration.nix ];
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
     loader = {
