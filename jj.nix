90186cdcac25a1da98a56fbcb28e496dd6e942dc
ollerus <24956184+ollerus@users.noreply.github.com>
2018-11-04 00:47:27 +0300

    Update jj.nix

--- jj.nix
+++ jj.nix
@@ -1,16 +1,16 @@
 { config, pkgs, ... }:
 {
   imports = [ ./hardware-configuration.nix ];
-  nixpkgs.overlays = 
-  [ (self: super: {
-    buildLinux = (cfg: super.buildLinux rec {
-     version = "4.18";
-     modDirVersion = "4.18.0";
-     extraMeta.branch = "4.18";
-     src = super.fetchurl { url = "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.18.tar.xz"; sha256 = "19d8bcf49ef530cd4e364a45b4a22fa70714b70349c8100e7308488e26f1eaf1"; };
-     ignoreConfigErrors = true;
-     extraConfig = cfg;
-  ) ]
+  nixpkgs.overlays = [ 
+    (self: super: {
+      buildLinux = (cfg: super.buildLinux rec {
+        version = "4.18";
+        modDirVersion = "4.18.0";
+        extraMeta.branch = "4.18";
+        src = super.fetchurl { url = "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.18.tar.xz"; sha256 = "19d8bcf49ef530cd4e364a45b4a22fa70714b70349c8100e7308488e26f1eaf1"; };
+        ignoreConfigErrors = true;
+        extraConfig = cfg;
+    ) ]
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
