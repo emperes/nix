c779ed915c9db30b771b1868ddab2435bbf68770
ollerus <24956184+ollerus@users.noreply.github.com>
2018-11-04 01:53:14 +0300

    Update reiser4.nix

--- reiser4.nix
+++ reiser4.nix
@@ -6,12 +6,21 @@
   boot.loader.grub.device = "/dev/sda";
   boot.kernelPackages = pkgs.linuxPackages_custom rec { 
     version = "4.18.0";
-    configfile = /etc/nixos/kernel.config;
     src = pkgs.fetchurl {
       url = "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.18.tar.xz";
       sha256 = "19d8bcf49ef530cd4e364a45b4a22fa70714b70349c8100e7308488e26f1eaf1";
     };
+    configfile = /etc/nixos/kernel.config;
   };
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      reiser4 = pkgs.stdenv // {
+        platform = pkgs.reiser4.platform // {
+          kernelPatches = [ { patch=/home/obliq/reiser4-for-4.18.0.patch; name="reiser4"; }];
+        };
+      };
+    };
+  };      
   environment.systemPackages = with pkgs; [ libaal reiser4progs ];
   system.stateVersion = "18.09";
 }
