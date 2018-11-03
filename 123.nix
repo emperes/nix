f7fdd5b259c2e7165c950c256b28c656a0c16e97
ollerus <24956184+ollerus@users.noreply.github.com>
2018-11-03 15:00:41 +0300

    Update 123.nix

--- 123.nix
+++ 123.nix
@@ -1,21 +1,8 @@
 { config, pkgs, ... }:
 {
   imports = [ ./hardware-configuration.nix ];
-  nixpkgs.config.packageOverrides = pkgs: {
-    reiser4 = pkgs.reiser4.override {
-      kernelPatches = [
-        { patch=/home/obliq/reiser4-for-4.18.0.patch; name="reiser4"; } 
-      ];
-    };
-  };
-  boot = {
-    kernelModules = [ "reiser4" ];
-    supportedFilesystems = [ "reiser4" ];
-    loader.grub.enable = true;
-    loader.grub.version = 2;
-    loader.grub.device = "/dev/sda";
-    kernelPackages = pkgs.linuxPackages_custom { version = "4.18"; src = pkgs.fetchurl { url = "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.18.tar.gz"; };
-  };
+  nixpkgs.config.packageOverrides = pkgs: { reiser4 = pkgs.reiser4.override { kernelPatches = [ { patch=/home/obliq/reiser4-for-4.18.0.patch; name="reiser4"; }  ]; }; };
+  boot = { kernelModules = [ "reiser4" ]; supportedFilesystems = [ "reiser4" ]; loader.grub.enable = true; loader.grub.version = 2; loader.grub.device = "/dev/sda"; kernelPackages = pkgs.linuxPackages_custom { version = "4.18"; src = pkgs.fetchurl { url = "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.18.tar.gz"; }; };
   environment.systemPackages = with pkgs; [ reiser4prog libaal ];
   system.stateVersion = "18.09"; # Did you read the comment?
 }
