fee17b71c4c64a00fe0b8727c055806574207a6c
ollerus <24956184+ollerus@users.noreply.github.com>
2018-11-03 22:34:32 +0300

    Update 123.nix

--- 123.nix
+++ 123.nix
@@ -4,13 +4,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
-  boot.kernelPackages = with pkgs.linuxPackages_custom.overrideDerivation (oldAttr: {
-    version = "4.6.0-custom";
-    src = pkgs.fetchurl {
-      url = "mirror://kernel/linux/kernel/v4.x/linux-4.6.tar.xz";
-      sha256 = "0rnq4lnz1qsx86msd9pj5cx8v97yim9l14ifyac7gllabb6p2dx9";
-    };  
-  };
+  boot.kernelPackages = with pkgs.linuxPackages_custom.overrideDerivation (oldAttr: { version = "4.6.0-custom"; src = pkgs.fetchurl { url = "mirror://kernel/linux/kernel/v4.x/linux-4.6.tar.xz"; };  
   environment.systemPackages = with pkgs; [ libaal reiser4progs ];
   system.stateVersion = "18.09";
 }
