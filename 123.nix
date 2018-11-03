af9bd686c69df1dd0aac6255a5cfc8c2dd206554
ollerus <24956184+ollerus@users.noreply.github.com>
2018-11-03 14:34:13 +0300

    Update 123.nix

--- 123.nix
+++ 123.nix
@@ -1,56 +1,102 @@
-boot.kernelPackages  = pkgs.linuxPackages_custom {
-    version = "4.4.0-rc5";
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  boot.kernelPackages  = pkgs.linuxPackages_custom {
+    version = "4.18";
     src = pkgs.fetchurl {
-      url =
-"mirror://kernel/linux/kernel/v4.x/testing/linux-4.4-rc5.tar.xz";
-      sha256 = "0qr1a8nmq6csbsw4cbqnn3m37a0fapj7a7cm9vj7fy7kq1rgxkpb";
+      url = "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.18.tar.gz"
+  # Use the GRUB 2 boot loader.
+  nixpkgs.config.packageOverrides = pkgs: {
+    reiser4 = pkgs.reiser4.override {
+      kernelPatches = [
+        { patch=/home/obliq/reiser4-for-4.18.0.patch; name="reiser4";}
+      ];
     };
-    configfile = /etc/nixos/kernel-4.4.0-rc5/kernel-4.4.0-rc5.config;
-  };
+  };    
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-ran *nixos-rebuild switch* and got a shiny new kernel. Based on the same
-wiki page, I then added the following to my configuration.nix file hoping
-to apply kernel patches to the new kernel.
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
- nixpkgs.config = {
-   packageOverrides = pkgs: {
-     stdenv = pkgs.stdenv // {
-       platform = pkgs.stdenv.platform // {
-         kernelPatches = [
-  { patch=/etc/nixos/kernel-4.4.0-rc5/01_check_fwnode_type.patch;
-name="yoga900_01"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/02_rename_is.to_pset.patch;
-name="yoga900_02"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/03_refactor_builtin_prop.patch;
-name="yoga900_03"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/04_keep_single_value.patch;
-name="yoga900_04"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/05_helper_macro_prop.patch;
-name="yoga900_05"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/06_improve_readability.patch;
-name="yoga900_06"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/07_prop_not_found_ret_EINVAL.patch;
-name="yoga900_07"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/08_fallback-2_sec_fwnode.patch;
-name="yoga900_08"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/09_take_copy_prop_set.patch;
-name="yoga900_09"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/10_supp_builtin_prop.patch;
-name="yoga900_10"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/11_dont_overwrite_sce_fwnode.patch;
-name="yoga900_11"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/12_propagate_dev_prop_2_sub.patch;
-name="yoga900_12"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/13_pass_dev_params_supp.patch;
-name="yoga900_13"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/14_pass_sda_hold_time.patch;
-name="yoga900_14"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/15_pass_uart_conf.patch;
-name="yoga900_16"; }
-  { patch=/etc/nixos/kernel-4.4.0-rc5/16_convert_unified_dev_prop.patch;
-name="yoga900_16"; }
-         ];
-       };
-     };
-   };
-  };
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   wget vim
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.guest = {
+  #   isNormalUser = true;
+  #   uid = 1000;
+  # };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+}
