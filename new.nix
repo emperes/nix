d815f2583b661219575247d18f8bed99c3d430ca
ollerus <24956184+ollerus@users.noreply.github.com>
2018-11-19 16:11:51 +0000

    Create new.nix

---
+++ new.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+{
+  imports = [ ./ ]
+  boot = {
+    cleanTmpDir = true;
+    kernelModules = [ "kvm-intel" "fuse" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    supportedFilesystems = [ "ntfs-3g" ];
+    loader = {
+      #efi.efiSysMountPoint = "/boot/efi";
+      grub = {
+        #efiSupport = true;
+        #efiInstallAsRemovable = true;
+        #useOSProber = true;
+        enable = true;
+        version = 2;
+        device = "/dev/sda"; }; }; };
+}
