7533d3d1df189530c36602efbb2a0e130ef6b5b2
ollerus <24956184+ollerus@users.noreply.github.com>
2018-11-19 17:03:06 +0000

    Update new.nix

--- new.nix
+++ new.nix
@@ -2,6 +2,7 @@
 {
   imports = [ ./hardware-configuration.nix ];
   boot = {
+    plymouth.enable = true;
     cleanTmpDir = true;
     kernelModules = [ "kvm-intel" "fuse" ];
     kernelPackages = pkgs.linuxPackages_latest;
@@ -32,6 +33,7 @@
     opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ vaapiIntel libvdpau-va-gl vaapiVdpau ];
     opengl.s3tcSupport = true;
     pulseaudio.enable = true;
+    pulseaudio.package = pkgs.pulseaudioFull;
     pulseaudio.support32Bit = true;
     pulseaudio.systemWide = true; };
   i18n = {
@@ -52,26 +54,54 @@
       sansSerif = [ "Noto Sans" ];
       serif = [ "Noto Serif" ]; }; };
   networking = { hostName = "ollerus"; networkmanager.enable = true; };
-  environment.systemPackages = with pkgs; [ tmux mc unzip ntfs3g python2Full mate.engrampa
-                                            gnupg gnupg1compat gitFull cmake gnumake gcc8 
-                                            firefox vlc neofetch wget python3Full unar
-                                            djview xpdf rsync ffmpeg-full python37Full
-                                            chromium pavucontrol geany aqemu
-                                            fuse appimage-run libreoffice-fresh vscode
-                                            bison flex openssl curl apulse pamixer pamix
-                                            snappy libopus nss xorg.libxkbfile xorg.libXScrnSaver
-                                            harfbuzzFull gtk2-x11 gnome2.gtk flac libelf
-                                            xdg_utils gparted acpi bc acpitool htop ncurses
-                                            imagemagick speedcrunch links paprefs pasystray tor 
-                                            torsocks torbrowser playonlinux wineFull winetricks
-                                            audacity gnome3.gnome-sound-recorder wxcam ];
+  environment.systemPackages = with pkgs; ([
+    tmux mc unzip ntfs3g python2Full mate.engrampa
+    gnupg gnupg1compat gitFull cmake gnumake gcc8 
+    firefox vlc neofetch wget python3Full unar
+    djview xpdf rsync ffmpeg-full python37Full
+    chromium pavucontrol geany aqemu pinentry
+    ripgrep scripts global coreutils
+    direnv emacs fasd fzf
+    fuse appimage-run libreoffice-fresh vscode
+    bison flex openssl curl apulse pamixer pamix
+    snappy libopus nss xorg.libxkbfile xorg.libXScrnSaver
+    harfbuzzFull gtk2-x11 gnome2.gtk flac libelf
+    xdg_utils gparted acpi bc acpitool htop ncurses
+    imagemagick speedcrunch links paprefs pasystray tor 
+    torsocks torbrowser playonlinux wineFull winetricks
+    audacity gnome3.gnome-sound-recorder wxcam 
+  ]
+  ++ lib.optionals stdenv.isLinux [
+    arandr autorandr blueman bluez cabal2nix
+    docker docker_compose dunst feh
+    haskellPackages.xmobar
+    haskellPackages.xmonad
+    haskellPackages.xmonad-contrib
+    haskellPackages.xmonad-extras
+    haskellPackages.yeganesh
+    iw keybase keybase-gui libnotify
+    lightum powertop rofi rofi-emoji
+    rofi-pass rofi-wifi-menu scrot
+    stalonetray unclutter-xfixes
+    wirelesstools xclip xsel
+    xorg.libXrandr xorg.xbacklight
+    xorg.xcursorthemes xorg.xf86inputkeyboard zeal
+  ]
+  ++ lib.optionals stdenv.isDarwin [
+    chunkwm.border
+    chunkwm.core
+    chunkwm.ffm
+    chunkwm.tiling skhd
+    DashDocker
+  ]);
   virtualisation = { docker.enable = true; virtualbox.host.enable = true; libvirtd.enable = true; };
   services.xserver.enable = true;
   services.xserver.layout = "us,ru";
   services.xserver.xkbOptions = "grp:caps_toggle,grp_led:num"; # scroll,num,caps
   #services.xserver.libinput.enable = true;
-  services.xserver.desktopManager = {
-  services.xserver.displayManager = {
+  services.xserver.windowManager = { default = "xmonad"; xmonad = { enable = true; enableContribAndExtras = true; }; };
+  services.xserver.desktopManager = { xterm.enable = false; default = "none"; };
+  services.xserver.displayManager.auto = { enable = true; user = "obliq"; };
   services.xserver.videoDrivers = [ "intel" ];
   services.xserver.synaptics.enable = true;
   services.actkbd.enable = true;
@@ -79,6 +109,7 @@
   services.compton.enable = true;
   services.compton.vSync = "opengl";
   services.acpid.enable = true;
+  services.thermald.enable = true;
   services.tlp.enable = true;
   services.illum.enable = true;
   services.printing.enable = true;
@@ -90,19 +121,31 @@
   services.dbus = { enable = true; packages = with pkgs; [ gnome2.GConf ]; };
   services.cron.enable = true;
   services.locate.enable = true;
+  services.dunst.enable = true;
+  services.autorandr.enable = true;
+  keyservices.autocutsel.enable = true;
   services.udisks2.enable = true;
+  services.keybase.enable = true;
+  services.kbfs = { enable = true; mountPoint = "/keybase"; };
   services.ntp.enable = true;
   services.tor.enable = true;
   system = { 
     autoUpgrade.enable = true;
     stateVersion = "unstable";
     autoUpgrade.channel = https://nixos.org/channels/nixos-unstable; };
-  nix = { gc.automatic = true; autoOptimiseStore = true; useSandbox = true; };
-  nixpkgs.config = { allowBroken = true; allowUnfree = true; };
+  nix = { 
+    gc.automatic = true; 
+    autoOptimiseStore = true; 
+    useSandbox = true;
+    binaryCaches = [ https://cache.nixos.org https://peel.cachix.org ];
+    trustedUsers = [ "obliq" "root" ];
+    };
+  nixpkgs.config = { allowUnsupportedSystem = false; allowBroken = true; allowUnfree = true; };
   programs = {
     java.enable = true;
     mtr.enable = true;
     adb.enable = true;
+    light.enable = true;
     zsh = {
       enable = true;
       enableCompletion = true;
@@ -122,7 +165,16 @@
     shell = pkgs.zsh;
     extraGroups = [ "audio" "adb" "adbuser" "docker" "host"
                     "pulse" "video" "wheel" "vboxusers" "systemd-journal"
-                    "libvirtd" "docker" "virtualisation" ]; };
+                    "libvirtd" "docker" "virtualisation" "networkmanager"
+                    "disk" "messagebus" ]; };
+  security.sudo.enable = true;
+  security.sudo.wheelNeedsPassword = false;
+  security.sudo.extraConfig = ''
+    ALL ALL = (root) NOPASSWD: ${pkgs.iw}/bin/iw
+    ALL ALL = (root) NOPASSWD: ${pkgs.light}/bin/light
+    ALL ALL = (root) NOPASSWD: ${pkgs.systemd}/bin/shutdown
+    ALL ALL = (root) NOPASSWD: ${pkgs.systemd}/bin/reboot
+  '';
   systemd.services = {
     systemd-tmpfiles-setup.before = [ " sysinit.target " ];
     systemd-update-utmp.after = [ " systemd-tmpfiles-setup.service " ]; };
