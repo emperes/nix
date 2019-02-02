050f861532f6af93710e4cff7e894abeddb02318
emperes <24956184+emperes@users.noreply.github.com>
2019-02-02 18:52:17 +0300

    Update nix.nix

--- conf/nix.nix
+++ conf/nix.nix
@@ -8,10 +8,10 @@
      binaryCachePublicKeys = [ "emperes.cachix.org-1:NsQvrJD2N3fR2501w6cEESFkUQbOjzpmpsX5tC/s+sI=" ];
      trustedUsers = [ "obliq" "root" ]; };
      
-   nixpkgs.config = { allowBroken = true; allowUnfree = true; allowUnsupportedSystem = false; };
+   nixpkgs.config = { allowBroken = true; allowUnfree = true; allowUnsupportedSystem = false; android_sdk.accept_license = true; };
    
    system = { 
-    autoUpgrade.enable = true;
+    autoUpgrade.enable = false;
     stateVersion = "unstable";
     autoUpgrade.channel = https://nixos.org/channels/nixos-unstable; };
 }
