54ff143c9dba4b159c537f1efa15198e6a98a5c2
emperes <24956184+emperes@users.noreply.github.com>
2019-02-02 08:37:20 +0000

    Update boot.nix

--- conf/boot.nix
+++ conf/boot.nix
@@ -5,15 +5,14 @@
     kernelModules = [ "kvm-intel" "fuse" ]; #"reiser4" "spadfs" 
     kernelPackages = pkgs.linuxPackages_latest;
     supportedFilesystems = [ "ntfs-3g" ]; #"reiser4" "spadfs" 
-    loader = {
-      systemd-boot.enable = true;
-      efi = { canTouchEfiVariables = true; efi.efiSysMountPoint = "/boot/efi"; };
+    loader = { systemd-boot.enable = true; efi = { canTouchEfiVariables = true; };
+      efi.efiSysMountPoint = "/boot/efi";
       #grub = {
         #efiSupport = true;
         #efiInstallAsRemovable = true;
         #useOSProber = true;
         #enable = true;
         #version = 2;
-        #device = "/dev/sdX";
-   }; }; };
+        #device = "/dev/sdX"; }; 
+   }; };
 }
