5c8564a11f7bb0b7c5565e900556f371ef7affc0
emperes <24956184+emperes@users.noreply.github.com>
2019-02-02 13:13:27 +0000

    Create desktop.nix

---
+++ conf/desktop.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+{
+  services = {
+    compton = { enable = true; vSync = "opengl"; };
+    xserver = {
+      enable = true;
+      displayManager.auto = { enable = true; user = "obliq"; };
+      desktopManager = { xfce.enable = true; default = "xfce"; };
+}
