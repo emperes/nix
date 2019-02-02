bc7ab5c7f5675398365ca9f6fbabfc8680638e4d
emperes <24956184+emperes@users.noreply.github.com>
2019-02-02 07:59:07 +0000

    Update shell.nix

--- conf/shell.nix
+++ conf/shell.nix
@@ -1,23 +1,25 @@
 { pkgs, ... }:
 {
-   zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestions.enable = true;
-      syntaxHighlighting.enable = true;
-      syntaxHighlighting.highlighters = [ "main" "brackets" "pattern" "cursor" "root" "line" ];
-      interactiveShellInit = ''ZSH_TMUX_AUTOSTART=true
-                               ZSH_TMUX_AUTOQUIT=true
-                               ENABLE_CORRECTION="true"'';
-      ohMyZsh = {
-        enable = true;
-        theme = "ys";
-        plugins = [ "git" "tmux" "sudo" "systemd" "jira"
-                    "httpie" "rsync" "git-extras"
-                    "common-aliases" "pip" "python" "vscode" ]; }; };
+   programs = {
+     bash = { enableCompletion = true; };
+     zsh = {
+       enable = true;
+       enableCompletion = true;
+       autosuggestions.enable = true;
+       syntaxHighlighting.enable = true;
+       syntaxHighlighting.highlighters = [ "main" "brackets" "pattern" "cursor" "root" "line" ];
+       interactiveShellInit = ''ZSH_TMUX_AUTOSTART=true
+                                ZSH_TMUX_AUTOQUIT=true
+                                ENABLE_CORRECTION="true"'';
+       ohMyZsh = {
+         enable = true;
+         theme = "ys";
+         plugins = [ "git" "tmux" "sudo" "systemd" "jira"
+                     "httpie" "rsync" "git-extras"
+                     "common-aliases" pip python vscode ]; }; }; };
                     
-   environment.shellAliases = {
-    ns = "nix-shell --command zsh";
-    "XD" = ''echo "emperes"'';
-    nixi = ''nix repl "<nixpkgs>"''; };
+    environment.shellAliases = {
+     ns = "nix-shell --command zsh";
+     "XD" = ''echo "emperes"'';
+     nixi = ''nix repl "<nixpkgs>"''; };
 }
