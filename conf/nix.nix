2639b7bab3c9af30354f68d57e18039068748327
emperes <24956184+emperes@users.noreply.github.com>
2019-02-02 09:08:16 +0000

    Update nix.nix

--- conf/nix.nix
+++ conf/nix.nix
@@ -1,8 +1,16 @@
 { config, pkgs, ... }:
 {
-   nix = { gc.automatic = true; autoOptimiseStore = true; useSandbox = true; }; 
-   nixpkgs.config = { allowBroken = true; allowUnfree = true; };
+   nix = { 
+     gc.automatic = true;
+     autoOptimiseStore = true;
+     useSandbox = true;
+     binaryCaches = [ "https://cache.nixos.org/" "https://ollerus.cachix.org" ];
+     binaryCachePublicKeys = [ "emperes.cachix.org-1:NsQvrJD2N3fR2501w6cEESFkUQbOjzpmpsX5tC/s+sI=" ];
+     trustedUsers = [ "obliq" "root" ];
+     }; 
+   nixpkgs.config = { allowBroken = true; allowUnfree = true; allowUnsupportedSystem = false; };
    system = { 
     autoUpgrade.enable = true;
     stateVersion = "unstable";
     autoUpgrade.channel = https://nixos.org/channels/nixos-unstable; };
+}
