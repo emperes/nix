64baf2a2fc4eaee479209e74ab51974f37a0c2ae
emperes <24956184+emperes@users.noreply.github.com>
2019-02-02 18:51:27 +0300

    Update network.nix

--- conf/network.nix
+++ conf/network.nix
@@ -4,11 +4,4 @@
     hostName = "emperes";
     networkmanager.enable = true;
     timeServers = [ "0.ca.pool.ntp.org" "1.ca.pool.ntp.org" "2.ca.pool.ntp.org" "3.ca.pool.ntp.org" ]; };
-    
-    #wireless.enable = true; # wpa
-    #firewall.allowedTCPPorts = [ ... ];
-    #firewall.allowedUDPPorts = [ ... ];
-    #firewall.enable = false;
-    #proxy.default = "http://user:password@proxy:port/";
-    #proxy.noProxy = "127.0.0.1,localhost,internal.domain"; };
-}    
+}
