d39ba823d71f976e9082d3f9311d468fa1ca5523
emperes <24956184+emperes@users.noreply.github.com>
2019-02-02 07:53:50 +0000

    Update shell.nix

--- conf/shell.nix
+++ conf/shell.nix
@@ -14,10 +14,10 @@
         theme = "ys";
         plugins = [ "git" "tmux" "sudo" "systemd" "jira"
                     "httpie" "rsync" "git-extras"
-                    "common-aliases" pip python vscode ]; }; }; };
+                    "common-aliases" pip python vscode ]; }; };
                     
    environment.shellAliases = {
     ns = "nix-shell --command zsh";
     "XD" = ''echo "emperes"'';
     nixi = ''nix repl "<nixpkgs>"''; };
- }
+}
